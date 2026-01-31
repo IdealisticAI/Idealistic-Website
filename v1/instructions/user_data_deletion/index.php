56e0e7663aea1fdf8265eb8e409bafcbe0086e3d
Evangelos <vaggdedes@gmail.com>
2026-01-31 20:46:23 +0200

    Update index.php

--- v1/instructions/user_data_deletion/index.php
+++ v1/instructions/user_data_deletion/index.php
@@ -4,6 +4,7 @@ header('Content-type: text/plain');
 echo "USER DATA DELETION\n";
 echo "Effective Date: 6th of September 2025\n\n";
 echo "Please email us and attach any relevant details such as email address, usernames, full names, e.t.c you have used with our services.\n";
+echo "We may ask for additional information or additional processes to be completed to truly verify it is you creating this request.\n";
 echo "We will find any correlated information and process your request as soon as possible, confirming to you once the deletion has been completed.\n\n";
 
 echo "CONTACT US\n";
